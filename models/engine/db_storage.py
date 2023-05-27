66248214ed37fb8583ce51e720de35347c7ac824
cruwmorgan <morganokeke@gmail.com>
2023-05-26 23:59:27 -0700

    Update DBStorage and FileStorage in the branch storage_get_count:

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,20 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID,
+        or None if not found"""
+        if type(cls) == str:
+            cls = classes.get(cls)
+        if cls is None or id is None:
+            return ('None')
+        return self.__session.query(cls).filter(cls.id == id).first()
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage:"""
+        if type(cls) is str:
+            cls = classes.get(cls)
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
